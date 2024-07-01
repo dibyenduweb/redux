8547af06671ec39216784b6f7b917cf529e4f5e0
dibyenduweb <chottu668@gmail.com>
2024-07-01 21:19:59 +0530

    update

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,29 @@
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
+import { deleteTodo } from "../features/todo/todoSlice";
 
 export default function Todo() {
     const todos = useSelector((state) => state.todos);
     console.log(todos);
-  return (
+   
+    const dispatch = useDispatch()
+
+    const clickHandler = (id) => {
+      console.log("delete", id);
+      dispatch(deleteTodo(id));
+
+    }
+  
+  
+    return (
     <>
       <h3>Todo</h3>
       <ul>
         {todos.map((todo) => (
-            <li key={todo.id}>{todo.task}</li>
+            <li key={todo.id}>{todo.task}
+            <button onClick={() => clickHandler(todo.id)}>Delete</button> 
+            </li>
         ))}
       </ul>
     </>
